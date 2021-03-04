0f20949770b8b6bbfacd3bccae9d79c990408237
Rafael <rafael@thinkpad.io>
2021-03-03 21:11:19 -0600

    Update speedtest-cli.sh

--- debian/speedtest-cli.sh
+++ debian/speedtest-cli.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
-# script to install speedtest.net cli tool
+# script to install speedtest.net cli tool on debian
 # still working on this, so right now just basic commands below
 
 # install dependencies
-apt-get -y install gnupg1 apt-transport-https dirmngr lsb-release
+apt-get install gnupg1 apt-transport-https dirmngr
 
 # set key
 export INSTALL_KEY=379CE192D401AB61
 
-# set current debian release
-export DEB_DISTRO=$(lsb_release -sc)
-
 # add key
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys $INSTALL_KEY
 
-# install repository 
-echo "deb https://ookla.bintray.com/debian ${DEB_DISTRO} main" | tee /etc/apt/sources.list.d/speedtest.list
+# set apt list file
+echo "deb https://ookla.bintray.com/debian generic main" | tee /etc/apt/sources.list.d/speedtest.list
 
-# update and install speedtest
+# update apt repos and install speedtest package
 apt-get update && apt-get -y install speedtest
