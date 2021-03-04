f3ec673a22e70e8b6e7232858966546ff63b794d
Rafael <rafael@thinkpad.io>
2021-03-03 21:20:21 -0600

    Update docker.sh

--- debian/docker.sh
+++ debian/docker.sh
@@ -6,22 +6,24 @@
 apt-get update
 
 # install dependencies
-apt-get -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common lsb-release
+apt-get install apt-transport-https ca-certificates curl gnupg
 
 # add Docker’s official GPG key
-curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
 # add Docker repo
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list
 
 # update apt again to fetch new docker repo
 apt-get update
 
 # install docker et all 
-apt-get -y install docker-ce docker-ce-cli containerd.io
+apt-get install docker-ce docker-ce-cli containerd.io
 
 # download latest docker-compose binary and move it to /usr/local/bin
-curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.28.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
 # make binary file executable
 chmod +x /usr/local/bin/docker-compose
